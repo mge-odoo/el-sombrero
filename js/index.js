36302388336bfa703db52fa1d31b146e2efebe3c
Corentin Lamy <lamy.corentin@gmail.com>
2015-12-28 15:23:26 +0100

    Update menu + Section info
    Section info plus claire + accès rapide aux infos importantes
    Rajout d'ingrédients dans le menu

--- js/index.js
+++ js/index.js
@@ -10,12 +10,13 @@ $(function() {
 
 	// Menu highlighting
 	var menu = $('#s_menu').offset().top;
-	var nourriture_saine = $('#s_visite_guidee').offset().top;
+	var visite = $('#s_visite_guidee').offset().top;
+	var nourriture_saine = $('#s_n_saine').offset().top;
 	var apropos = $('#s_apropos').offset().top;
 	var infos = $('#s_infos').offset().top;
 	var nav_offset = 120;
 	menu = menu - nav_offset;
-	nourriture_saine = nourriture_saine - nav_offset;
+	visite = visite - nav_offset;
 	apropos = apropos - nav_offset;
 	infos = infos - nav_offset;
 	
@@ -23,22 +24,22 @@ $(function() {
 		var y_scroll_pos = window.pageYOffset;
 		if (y_scroll_pos >= 0 && y_scroll_pos < menu) {
 			highlight_menu($('.navigation a')[0]);
-		} else if (y_scroll_pos >= menu && y_scroll_pos < nourriture_saine) {
+		} else if (y_scroll_pos >= menu && y_scroll_pos < visite) {
 			highlight_menu($('.navigation a')[1]);
+		} else if (y_scroll_pos >= visite && y_scroll_pos < nourriture_saine) {
+			highlight_menu($('.navigation a')[2]);
 		} else if (y_scroll_pos >= nourriture_saine && y_scroll_pos < apropos) {
-			 highlight_menu($('.navigation a')[2]);
-		} else if (y_scroll_pos >= apropos  && y_scroll_pos < infos) {
 			 highlight_menu($('.navigation a')[3]);
-		}else if (y_scroll_pos > infos) {
+		} else if (y_scroll_pos >= apropos  && y_scroll_pos < infos) {
 			 highlight_menu($('.navigation a')[4]);
+		}else if (y_scroll_pos > infos) {
+			 highlight_menu($('.navigation a')[5]);
 		}
 	});
 
 	$('.carousel').carousel({
 	  interval: 5000
 	});
-
-
 	$('.ingr_description').first().show();
 	
 	$('img.im_ingredient_small').hover(function(ev) {
@@ -63,18 +64,6 @@ $(function() {
 
 		// TODO: send the mail
 	});
-
-	// $('.valeur_img').on('mouseenter', function(ev) {
-	//      var id = $(ev.currentTarget).data('id');
-	//      var $textToDisplay = $('.valeur_text[data-id=' + id +']');
-	//      $textToDisplay.removeClass('invisible');
-	// });
-
-	// $('.valeur_img').on('mouseleave', function(ev) {
-	//      var id = $(ev.currentTarget).data('id');
-	//      var $textToDisplay = $('.valeur_text[data-id=' + id +']');
-	//      $textToDisplay.addClass('invisible');
-	// });
 });
 function ungray_out_background(td) {
 	document.getElementById(td.id).style.backgroundColor = "";
